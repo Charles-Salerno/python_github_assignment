2453be601dd22b89844c33fa4aa62940c841df48
Charles Salerno <ccsalerno@usf.edu>
2025-11-18 15:23:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+# Daily Study Tracker Program
 print("Welcome to my study tracker!")
-
+# Ask for input
 hours = input("How many hours did you study today? ")
-
+#Error handling
 try:
   hours = float(hours)
 except ValueError:
   print("Please enter a valid number. ")
   exit()
-
+# Calculation
 weekly = hours * 7
-
+# Output
 print(f"If you study like this every day, you'll study about {weekly} hours this week.")
